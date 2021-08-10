e0084e4f3774a555a13cf8441d636223d1b82839
natbibi <natalie.wuwu@googlemail.com>
2021-08-10 23:08:06 +0100

    :recycle: refactor code - split up and move graphQL related code (SoC)

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,9 @@
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
+
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query
+    }
+}
---
+++ graphql/typeDefs.js
@@ -0,0 +1,13 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+    type Query{
+        getPosts: [Post]
+    }
+`;
