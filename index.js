89642043e3eaa5181e7c5165442cf6b283a77622
natbibi <natalie.wuwu@googlemail.com>
2021-08-10 23:09:15 +0100

    :recycle: refactor code - split up and move graphQL related code (SoC) to own folder

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const { ApolloServer} = require('apollo-server');
-const gql = require('graphql-tag');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
-
-const typeDefs = gql`
-    type Query{
-        sayHi: String!
-    }
-`
-
-const resolvers = {
-    Query: {
-        sayHi: () => 'Hello World!'
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 });
-
 
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
