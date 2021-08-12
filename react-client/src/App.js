4a7bbee42b8f0115c045dcab1b1e3116783e09c2
natbibi <natalie.wuwu@googlemail.com>
2021-08-12 18:20:40 +0100

    :building_construction: render components Home and Profile components with protected routes, plus render layout items

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+import { Loading } from "./components";
+import { NavBar, Footer } from "./layout"
+import { Home, Profile } from "./views";
+import ProtectedRoute from "./auth/protected-route";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <div className="container flex-grow-1">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <ProtectedRoute path="/profile" component={Profile} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
