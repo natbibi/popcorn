518b4fa386f9ac9e8613a8cb5ab73e9748abf432
natbibi <natalie.wuwu@googlemail.com>
2021-08-15 19:24:29 +0100

    :alien: add queries to get all posts and get post by id

--- api/graphql/resolvers/posts.js
+++ api/graphql/resolvers/posts.js
@@ -1,14 +1,28 @@
-const { Post } = require('../../models/Post');
+const Post = require('../../models/Post');
 
 module.exports = {
     Query: {
-        async getPosts(){
+        async getPosts() {
             try {
-                const posts = await Post.find();
+                console.log('looking for posts in database')
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
-            } catch(err){
+            } catch (err) {
+                throw new Error("What post??");
+            }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findbyId(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found :(')
+                }
+            } catch (err) {
                 throw new Error(err);
             }
         }
-    }
-}
+    },
+    
+};
