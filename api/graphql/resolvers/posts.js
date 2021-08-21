ed89600f7a4e8eb793182e26e6bedbe70cadb7f3
natbibi <natalie.wuwu@googlemail.com>
2021-08-21 14:15:57 +0100

    :ambulance: add userId to createPost and deletePost

--- api/graphql/resolvers/posts.js
+++ api/graphql/resolvers/posts.js
@@ -1,10 +1,12 @@
+const { UserInputError } = require('apollo-server');
+
 const Post = require('../../models/Post');
+const User = require('../../models/User')
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                console.log('looking for posts in database')
                 const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (err) {
@@ -25,29 +27,49 @@ module.exports = {
         }
     },
     Mutation: {
-        async createPost(_, { body }) {
-            const newPost = new Post({
-                body,
-                user: user.id,
-                username: user.username,
-                createdAt: new Date().toISOString()
-            });
-            const post = await newPost.save();
-            return post;
-        },
-        async deletePost(_, { postId }){
+        async createPost(_, { userId, body }) {
+            const user = await User.findById(userId)
 
-            try {
-                const post = await Post.findById(postId);
-                if (user.username === post.username) {
-                    await post.delete()
-                    return "Post deleted successfully!";
-                } else {
-                    throw new Error("Not allowed!")
-                }
-            } catch(err) {
-                throw new Error(err);
+            if (user) {
+                const newPost = new Post({
+                    body,
+                    user: user.id,
+                    username: user.username,
+                    createdAt: new Date().toISOString()
+                });
+                const post = await newPost.save();
+                return post;
+            } else {
+                throw new UserInputError('Sorry..... try again!')
+        
             }
+    },
+    async deletePost(_, { postId, userId }) {
+        const user = await User.findById(userId)
+
+        try {
+            const post = await Post.findById(postId);
+            if (user.username === post.username) {
+                await post.delete()
+                return "Post deleted successfully!";
+            } else {
+                throw new Error("Not allowed!")
+            }
+        } catch (err) {
+            throw new Error(err);
         }
-    }
+    },
+    
+}
 };
+
+// mutation {
+//     createPost(userId: "61196045ef43c54666c1850e" body: "Lalala post post whooooppp") {
+//       id
+//       body
+//     }
+//   }
+
+// mutation($deletePostPostId: ID!) {
+//     deletePost(postId: "6120f78be8b38a7dc6535dbb" userId: "61196045ef43c54666c1850e")
+//   }
