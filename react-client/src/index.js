377b6c84462895dc65d5172b3c2b825c6028150a
natbibi <natalie.wuwu@googlemail.com>
2021-08-22 17:20:57 +0100

    :wrench: import and add ApolloProvider with necessary config (for local dev) and wrap around app

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'https://localhost:5000',
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Auth0ProviderWithHistory>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </Auth0ProviderWithHistory>
     </Router>
   </React.StrictMode>,
