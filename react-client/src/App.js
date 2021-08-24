12aab37283dd8798d062f40872bea416bd715cad
natbibi <natalie.wuwu@googlemail.com>
2021-08-24 21:31:44 +0100

    :lipstick: create a first css stylesheet and import to App / import semantic UI stylesheet

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
 import { Loading } from "./components";
 import { NavBar, Footer } from "./layout"
 import { Home, Profile } from "./views";
