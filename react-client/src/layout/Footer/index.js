9a2967919a2fb74688b5768b834bb7ae0cdfe2ba
natbibi <natalie.wuwu@googlemail.com>
2021-08-24 21:32:50 +0100

    :lipstick: positionfix footer to bottom, add alignment and some basic styling

--- react-client/src/layout/Footer/index.js
+++ react-client/src/layout/Footer/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Footer = () => (
-    <footer className="bg-light p-3 text-center">
-        <div className="logo" />
+    <footer className="ui inverted vertical footer segment" style={{ position: "fixed", width: "100%", bottom: "0", textAlign: "center" }}>
+        <div className="logo"/>
         <p>
             Made with ✨ by{" "}
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/natbibi">
