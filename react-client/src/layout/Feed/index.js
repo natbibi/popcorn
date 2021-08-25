622a0af61930b23ae3a9dab8c908fa16abbd320e
natbibi <natalie.wuwu@googlemail.com>
2021-08-25 21:51:42 +0100

    :sparkles: render list of Posts, map and send to Post component

--- react-client/src/layout/Feed/index.js
+++ react-client/src/layout/Feed/index.js
@@ -1,12 +1,34 @@
 import React from "react";
+import { useQuery } from '@apollo/client';
+import { Grid, Transition } from 'semantic-ui-react';
+
+import { Posts } from '../../components';
+import { FETCH_POSTS_QUERY } from '../../graphql';
+
 
 const Feed = () => {
+    const { loading, error, data } = useQuery(FETCH_POSTS_QUERY);
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
     return (
-       <>
-       <aside className="feed-container">
-           Middle
-       </aside>
-       </>
+        <>
+            <main className="feed-container">
+                {loading ? (
+                    <h1>Loading posts..</h1>
+                ) : (
+                    <Transition.Group>
+                        {data &&
+                            data.getPosts.map((post) => (
+                                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                    <Posts post={post} />
+                                </Grid.Column>
+                            ))}
+                    </Transition.Group>
+                )}
+            </main>
+        </>
     );
 };
 
