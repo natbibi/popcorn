5984da2875811c8e8739e97ef87c628062e1b75a
natbibi <natalie.wuwu@googlemail.com>
2021-08-26 21:12:41 +0100

    :lipstick: create cards to show fetched post data

--- react-client/src/components/Posts/index.js
+++ react-client/src/components/Posts/index.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Button, Label, Icon } from 'semantic-ui-react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 const Posts = ({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) => {
-    console.log(body)
+    console.log(likeCount)
+
+    function likePost() {
+        console.log('like post')
+    }
+
+    function addComment() {
+        console.log('commented')
+    }
+
     return (
         <>
             <Card fluid color="red">
                 <Card.Content>
-                    <Card.Header>{username}</Card.Header>
                     <Image
                         floated='right'
                         size='mini'
                         src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
                     />
-                    {body && body}
+                    <Card.Header>{username}</Card.Header>
+                    <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
+                    <Card.Description>{body}</Card.Description>
 
                 </Card.Content>
-            </Card>
+                <Card.Content extra>
+                    <Button as='div' labelPosition='right' onClick={likePost}>
+                        <Button color='red' basic>
+                            <Icon name='heart' />
+                        </Button>
+                        <Label basic color='red' pointing='left'>
+                            {likeCount}
+                        </Label>
+                    </Button>
 
+                    <Button as='div' labelPosition='right' onClick={addComment}>
+                        <Button color='red' basic>
+                            <Icon name='comment' />
+                        </Button>
+                        <Label basic color='red' pointing='left'>
+                            {commentCount}
+                        </Label>
+                    </Button>
+                </Card.Content>
+            </Card>
         </>
     )
 }
