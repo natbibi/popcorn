73bd5cbfcf75f5cff9141d245c5d26c85adb0ddf
natbibi <natalie.wuwu@googlemail.com>
2021-08-27 17:13:20 +0100

    :pencil2: fix query - now likeCount and commentCount show

--- react-client/src/graphql.js
+++ react-client/src/graphql.js
@@ -1,23 +1,23 @@
 import { gql } from '@apollo/client';
 
 export const FETCH_POSTS_QUERY = gql`
-query posts{
-  getPosts {
-    id
-    body
-    createdAt
-    username
-    likes {
+  {
+    getPosts {
       id
-      createdAt
-      username
-    }
-    comments{
-      id
-      createdAt
-      username
       body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
     }
   }
-}
 `;
