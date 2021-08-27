3963aec430c76e35b8a53177b113495c4b3f8260
natbibi <natalie.wuwu@googlemail.com>
2021-08-27 17:15:52 +0100

    :sparkles: functioning createPost form which createsPost for user in database and pulls the updated list
    from cache

--- react-client/src/components/CreatePost/index.js
+++ react-client/src/components/CreatePost/index.js
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from 'semantic-ui-react';
 import { gql, useMutation } from '@apollo/client'
+import { useAuth0 } from "@auth0/auth0-react";
 
-import { FETCH_POSTS_QUERY } from '../../graphql'
+import { FETCH_POSTS_QUERY } from '../../graphql';
 
 const CreatePost = () => {
+    const { user } = useAuth0();
+    const { sub } = user;
+    const mongoId = sub.substring(6)
 
+    const [values, setValues] = useState({ body: "", userId: mongoId });
+
+    const onChange = (e) => {
+        setValues(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        sendFormData();
+    };
+
+    const [sendFormData, { error }] = useMutation(CREATE_POST_MUTATION, {
+        variables: values,
+        update(proxy, result) {
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            });
+            let newData = [...data.getPosts];
+            newData = [result.data.createPost, ...newData];
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data: {
+                    ...data,
+                    getPosts: {
+                        newData,
+                    },
+                },
+            });
+            values.body = '';
+        }
+    })
+
+
+
+    console.log(JSON.stringify(error, null, 2));
 
     return (
         <>
 
-            <Form fluid>
+            <Form onSubmit={onSubmit}>
                 <h4>Share your thoughts!</h4>
                 <Form.Field>
                     <Form.Input
                         placeholder="What have you been watching? 🎬"
                         name="body"
+                        autoComplete="off"
+                        value={values.body}
+                        onChange={onChange}
+                        error={error ? true : false}
                     />
                     <Button type="submit" >
                         Post!
@@ -27,9 +70,10 @@ const CreatePost = () => {
     );
 };
 
+
 const CREATE_POST_MUTATION = gql`
-  mutation createPost($body: String!) {
-    createPost(body: $body) {
+  mutation createPost($body: String!, $userId: ID!) {
+    createPost(body: $body, userId: $userId) {
       id
       body
       createdAt
