58a4ce985fae49c9870a459166b954290dfe5184
natbibi <natalie.wuwu@googlemail.com>
2021-08-29 21:38:28 +0100

    :sparkles: route for search component at /search

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -7,43 +7,42 @@ import './App.css';
 
 import { Loading, SinglePost } from "./components";
 import { NavBar, Footer, SideBar, RightSideBar } from "./layout"
-import { Home, Search } from "./views";
+import { Home, SearchPage } from "./views";
 import ProtectedRoute from "./auth/protected-route";
 
 const App = () => {
-  const { isLoading } = useAuth0();
+	const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <Loading />;
-  }
+	if (isLoading) {
+		return <Loading />;
+	}
 
-  function ScrollToTop() {
-    const { pathname } = useLocation();
+	function ScrollToTop() {
+		const { pathname } = useLocation();
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
+		useEffect(() => {
+			window.scrollTo(0, 0);
+		}, [pathname]);
 
-    return null;
-  }
+		return null;
+	}
 
-  return (
-    <>
-      <NavBar />
-      <main className="main-layout">
-        <SideBar />
-        <ScrollToTop />
-        <Switch>
-          <ProtectedRoute path="/" exact component={Home} />
-          {/* <ProtectedRoute path="/profile" component={Profile} /> */}
-          <ProtectedRoute exact path={`/posts/:postId`} component={SinglePost} />
-          <Route exact path="/search" component={Search} />
-        </Switch>
-        <Footer />
-        <RightSideBar />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<main className="main-layout">
+				<SideBar />
+				<ScrollToTop />
+				<Switch>
+					<ProtectedRoute path="/" exact component={Home} />
+					<ProtectedRoute exact path={`/posts/:postId`} component={SinglePost} />
+					<Route exact path="/search" component={SearchPage} />
+				</Switch>
+				<Footer />
+				<RightSideBar />
+			</main>
+		</>
+	);
 };
 
 export default App;
