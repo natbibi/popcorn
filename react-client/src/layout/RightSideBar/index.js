4e84bb4753c8943afe9cede2eb25b05dcb6e6dc8
natbibi <natalie.wuwu@googlemail.com>
2021-08-29 16:41:45 +0100

    :alien: fetch on load list of trending shows from TMDB and save to state

--- react-client/src/layout/RightSideBar/index.js
+++ react-client/src/layout/RightSideBar/index.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+
 
 const RightSideBar = () => {
+    const [trendingData, setTrendingData] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        async function fetchTrending() {
+            try {
+                const token = process.env.REACT_APP_TOKEN;
+                const options = {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": `application/json;charset=utf-8`
+                    }
+                };
+                const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day`, options)
+                if (data.err) {
+                    throw new Error(data.err)
+                }
+                setTrendingData(data.results)
+            } catch {
+                console.warn("There's an error!!! Cannot fetch data!")
+                setError('❌ Error loading list')
+            }
+        } fetchTrending();
+    }, []);
+
     return (
-       <>
-       <aside className="sidebar-container">
-           Right Side 
-       </aside>
-       </>
+        <>
+            <aside className="sidebar-container right">
+                <h3>Trending Today </h3>
+              
+            </aside>
+        </>
     );
 };
 
