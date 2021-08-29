ce6d3b0400ecfcffb3f0c6e4ce83a0092218fd20
natbibi <natalie.wuwu@googlemail.com>
2021-08-29 19:16:54 +0100

    :recycle: refactor to get rid of duplicate prop error message

--- react-client/src/components/Posts/index.js
+++ react-client/src/components/Posts/index.js
@@ -35,15 +35,16 @@ const Posts = ({ post: { body, createdAt, id, username, likeCount, commentCount,
                 </Card.Content>
                 <Card.Content extra>
                     <LikeButton user={user} post={{ id, likes, likeCount }} />
-
-                    <Button as='div' labelPosition='right' as={Link} to={`/posts/${id}`}>
-                        <Button color='pink' basic>
-                            <Icon name='comments' />
+                    <Link to={`/posts/${id}`}>
+                        <Button as='div' labelPosition='right'>
+                            <Button color='pink' basic>
+                                <Icon name='comments' />
+                            </Button>
+                            <Label basic color='pink' pointing='left'>
+                                {commentCount}
+                            </Label>
                         </Button>
-                        <Label basic color='pink' pointing='left'>
-                            {commentCount}
-                        </Label>
-                    </Button>
+                    </Link>
                     {user && user.nickname === username && <DeleteButton postId={id} mongoId={mongoId} />}
                 </Card.Content>
             </Card>
