4dba4eb4f3fe6f28b737d2d796ce40820a775b3a
natbibi <natalie.wuwu@googlemail.com>
2021-08-29 22:24:35 +0100

    :construction: dropdown which changes view of fetched API data

--- react-client/src/views/SearchPage/index.js
+++ react-client/src/views/SearchPage/index.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Search } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 import axios from 'axios';
 
 import { PopularList } from "../../components";
 
 const SearchPage = () => {
-    const [inputData, setInputData] = useState({ input: "" });
+    const [inputData, setInputData] = useState("popularity.desc");
     const [error, setError] = useState();
-    const [searchResults, setSearchResults] = useState();
+    const [searchResults, setSearchResults] = useState([]);
+   
+    // const onChange = (e) => {
+    //     setInputData(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    //     console.log(setInputData)
+    // };
 
-    const onSearchChange = (e) => {
-        setInputData(prev => ({ ...prev, [e.target.name]: e.target.value }));
-    };
+    console.log(inputData)
+
+    const options = [
+        {
+            key: 'popularity.desc',
+            text: 'Most Popular',
+            value: 'popularity.desc'
+        },
+        {
+            key: 'popularity.asc',
+            text: 'Least Popular',
+            value: 'popularity.asc'
+        },
+    ]
+
 
     useEffect(() => {
         async function fetchPopular() {
@@ -23,7 +40,7 @@ const SearchPage = () => {
                         "Content-Type": `application/json;charset=utf-8`
                     }
                 };
-                const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`, options)
+                const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=${inputData}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`, options)
                 if (data.err) {
                     throw new Error(data.err)
                 }
@@ -34,18 +51,29 @@ const SearchPage = () => {
                 setError('Oops, please try again!')
             }
         } fetchPopular();
-    }, []);
+    }, [inputData]);
 
 
     return (
         <>
             <main className="search-container">
-                <Search
+                {/* <Search
                     name="input"
                     onSearchChange={onSearchChange}
-                    // onSubmit={onSubmit}
-                    value={inputData.input}
+                    onSubmit={onSubmit}
+                    value={inputData.input} 
+                />  */}
+
+                <Dropdown
+                    name="input"
+                    placeholder='Select View'
+                    fluid
+                    selection
+                    options={options}
+                    onSelect={(e, { value }) => setInputData(value)}
                 />
+
+
                 <h3>Popular</h3>
                 <section className="popular-container">
                     {searchResults && searchResults.slice(0, 20).map((r) => (
