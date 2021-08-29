983bb483e9e64a3760315c4e7ddc7af7589ceff7
natbibi <natalie.wuwu@googlemail.com>
2021-08-29 22:42:09 +0100

    :passport_control: add ternary to load something if user isnt logged in

--- react-client/src/layout/SideBar/index.js
+++ react-client/src/layout/SideBar/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { Profile } from '../../components'
 
 const SideBar = () => {
+    const { user } = useAuth0();
+
     return (
         <>
             <aside className="sidebar-container">
-                <Profile />
+                {user ?
+                    <Profile /> : <p>Please Wait...</p>
+                }
             </aside>
         </>
     );
