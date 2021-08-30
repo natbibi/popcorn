20f495c3a2ab62fff0073167f5d70695dceca78a
natbibi <natalie.wuwu@googlemail.com>
2021-08-30 12:57:13 +0100

    :clown_face: add mock data for hashtags section in sidebar

--- react-client/src/components/Profile/index.js
+++ react-client/src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -16,6 +17,18 @@ const Profile = () => {
 				avatar
 			/>
 			<h3>Hey, {nickname}!</h3>
+
+			<div className="hashtag-container">
+				<h4>Trending Hashtags</h4>
+				<p><Link>#D.P.</Link></p>
+				<p><Link>#HesAllThat</Link></p>
+				<p><Link>#TheChair</Link></p>
+				<p><Link>#Clickbait</Link></p>
+				<p><Link>#SuicideSquad</Link></p>
+				<p><Link>#Vagabond</Link></p>
+				<p><Link>#SuicideSquad</Link></p>
+				<p><Link>#Brooklyn99</Link></p>
+			</div>
 		</main>
 	);
 };
